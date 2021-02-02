602218397ab3327bde763f3ea3920d186ab66f73
Jee Ryann Genio <jeegenio@gmail.com>
2021-02-02 20:06:36 +0800

    added available phrase

--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -4,7 +4,7 @@ const Tours = ({tours,removeTour}) => {
 
   return <section>
     <div className="title">
-      <h2>our tours</h2>
+      <h2>our available tours</h2>
       <div className="underline"></div>
     </div>
     <div>
