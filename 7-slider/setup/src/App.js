ace218bed78e3dddab8872cd8e33dd81f3195d83
Jee Ryann Genio <jeegenio@gmail.com>
2021-02-10 21:54:11 +0800

    adding slide effects on app.js

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -6,6 +6,20 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] =useState(0);
 
+useEffect (() => {
+const lastIndex = people.length -1;
+if (index < 0) {
+  setIndex(lastIndex)
+}
+if (index > lastIndex){
+  setIndex(0);
+}
+}, [index, people])
+
+useEffect (() => {
+let slider = setInterval(() => {setIndex(index + 1)}, 5000);
+return ()=> clearInterval(slider);
+},[index])
   return <section className="section">
     <div className="title">
       <h2>
@@ -16,7 +30,14 @@ function App() {
         {people.map((person,personIndex)=> {
           const{id,image,name,title,quote} = person;
           //more stuff coming here
-let position = "nextSlide"
+let position = "nextSlide";
+if (personIndex === index) {
+  position = 'activeSlide'
+}
+if (personIndex === index-1 || (index === 0 && personIndex === people.length - 1))
+{
+  position = 'lastSlide'
+}
           return ( 
             <article className={position} key={id}>
               <img src={image} alt={name} className="person-img"/>
@@ -26,10 +47,10 @@ let position = "nextSlide"
               <FaQuoteRight className="icon"/>
           </article>
         )})}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index-1)}>
           <FiChevronLeft/>
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index+1)}>
           <FiChevronRight/>
         </button>
     </div>
